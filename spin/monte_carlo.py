02d268d4fe86c1ff22c147105e584e6e9a866fac
kmckiern <kmckiern@stanford.edu>
2017-09-10 21:12:14 -0700

    fix bugs, especially acf

--- spin/monte_carlo.py
+++ spin/monte_carlo.py
@@ -1,6 +1,7 @@
 import numpy as np
+from scipy.signal import correlate
 from .operators import *
-import matplotlib.pyplot as plt
+import copy
 
 """
 Sample system via MCMC with Gibbs Sampling
@@ -10,6 +11,7 @@ def flip_spin(configuration, geometry):
     """
     Flip random spin on lattice
     """
+    configuration = copy.copy(configuration)
     # identify random index
     flip_indices = []
     for dimension in geometry:
@@ -47,30 +49,29 @@ def MC_step(configuration, geometry, energy, T):
     else:
         return None
 
-def check_convergence(energies, threshold=1):
+def check_convergence(energies, threshold=.3):
     """
     Converged if standard error of the energy < threshold
     """
-    energies = energies[10:]
     ste = np.std(energies) / (len(energies)**.5)
     if ste < threshold:
         return False
     else:
         return True
 
-def check_autocorrelation(energies):
+def check_autocorrelation(energies, threshold=.01):
     """
     Determine autocorrelation of time series
     """
     energies -= np.mean(energies)
-    corr = np.correlate(energies, energies, mode='full')
-    n_samp = len(energies)
-    var = np.var(energies)
-    ac = corr / var
-    ac_l = int(ac.argmax() * .5)
-    return ac_l
+    n_samples = len(energies)
+    for lag in np.arange(0, n_samples, int(.05*n_samples)):
+        ac = np.corrcoef(energies[:n_samples-lag], energies[lag:n_samples])[0,1]
+        if ac < threshold:
+            return lag
+    return 0
 
-def run_MCMC(configuration, geometry, energy, T, n_samples=1, min_samples=10000):
+def run_MCMC(configuration, geometry, energy, T, eq=False, min_samples=10000):
     """
     Generate samples
         for mixing: until convergence criterion is met
@@ -89,7 +90,7 @@ def run_MCMC(configuration, geometry, energy, T, n_samples=1, min_samples=10000)
             configurations.append(configuration)
             energies.append(energy)
             if len(energies) > min_samples:
-                if n_samples == 1:
+                if eq:
                     # check convergence
                     continue_sampling = check_convergence(energies)
                     # if not converged, run for 10% more steps
@@ -100,21 +101,22 @@ def run_MCMC(configuration, geometry, energy, T, n_samples=1, min_samples=10000)
                 else:
                     # check autocorrelation
                     ac = check_autocorrelation(energies)
-                    print (ac)
-                    if len(configurations) > ac*n_samples:
+                    if ac == 0 or (len(configurations) < ac*n_samples):
+                        min_samples *= .1
                         continue
                     else:
-                        configs = configurations[::ac]                    
-                        return configs
+                        return configurations[::ac]
 
-def sample(config, geo, energy, T, n_samples=3):
+def sample(config, geo, energy, T, n_samples=100):
     """
     Run MCMC scheme on initial configuration
     """
     # equilibrate (mix) the chain
-    config, energy = run_MCMC(config, geo, energy, T)
+    config, energy = run_MCMC(config, geo, energy, T, eq=True, min_samples=50000)
 
     # get n_samples samples
-    configurations = run_MCMC(config, geo, energy, T, n_samples)
+    configurations = run_MCMC(config, geo, energy, T)
 
-    return configurations
+    # return ensemble of samples
+    return np.array(configurations)
+
