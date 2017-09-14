1154c74018ba7e70b4b153d8bb3e1e765c5a02c4
kmckiern <kmckiern@stanford.edu>
2017-09-13 22:03:20 -0700

    pep8-ier

--- spin/system.py
+++ spin/system.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.ndimage import filters 
 
 class System(object):
+
     """
     Representation of a binary particle system
 
@@ -12,10 +13,13 @@ class System(object):
     geometry : geometric arrangement of particles
     configuration : spin configuration of particles
     """
+
     def __init__(self, T=1, spin=1, geometry=(1,), configuration=None):
+
         """
         Returns particle system object 
         """
+
         self._T = T
         self._spin = spin
         if isinstance(geometry, int):
@@ -27,16 +31,20 @@ class System(object):
             self._configuration = configuration
 
     def random_configuration(self):
+
         """
         Distribute particles according to random configuration
         """
+
         state = np.random.choice([-1, 1], size=self._geometry)
         state *= self._spin
         return state
 
     def uniform_configuration(self, val):
+
         """
         Distribute particles according to uniform configuration
         """
+
         state = np.ones(self._geometry) * val
         return state
