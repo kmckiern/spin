4a3e6e2a88185718815f4d32fd476ff43f3a8f23
kmckiern <kmckiern@stanford.edu>
2017-09-14 12:34:17 -0700

    for quick testing

--- interactive_system.py
+++ interactive_system.py
@@ -4,8 +4,12 @@ from spin import Model
 import IPython
 
 x = Model()
-x.generate_system(geometry=(6,6))
+
+x.generate_system(geometry=(6,6), T=3, configuration=np.load('samples/config.npy'))
 x.measure_system()
-x.generate_ensemble()
+
+x.generate_ensemble(n_samples=5)
+ec = x._ensemble._configurations
+x.measure_ensemble()
 
 IPython.embed()
