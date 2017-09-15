c40d74e5f65f07f444088065ac818eff9bc1606d
kmckiern <kmckiern@stanford.edu>
2017-09-15 16:53:52 -0700

    fuller discription of capabilities in example script

--- interactive_system.py
+++ interactive_system.py
@@ -1,15 +1,43 @@
 #!/bin/env python
 
 from spin import Model
+import os
+import numpy as np
 import IPython
 
+# lattice and system parameters
+geo = (6,6)
+temp = 3
+example_IC = os.path.join('.', 'samples/config.npy')
+
+# create model object
 x = Model()
 
-x.generate_system(geometry=(6,6), T=3, configuration=np.load('samples/config.npy'))
+# create a system, with or without an IC
+if os.path.exists(example_IC):
+    x.generate_system(geometry=geo, T=temp, configuration=np.load(example_IC))
+else:
+    x.generate_system(geometry=geo, T=temp)
+    x.random_configuration()
+
+# measure observables of created system
 x.measure_system()
 
+# generate ensemble from system
 x.generate_ensemble(n_samples=5)
+
+# view ensemble configurations as np array
 ec = x._ensemble._configurations
+
+# measure observables on ensemble
 x.measure_ensemble()
 
+# print discriptions of state and ensemble
+print('~ system description ~')
+print(x.describe_system())
+print ('\n\n\n\n')
+print('~ ensemble description ~')
+print(x.describe_ensemble())
+
+# start interactive session to work with system/ensemble
 IPython.embed()
