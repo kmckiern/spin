38516458b66c0aca4d8af9f71eaa489d1fff5d8f
kmckiern <kmckiern@stanford.edu>
2017-09-16 18:01:10 -0700

    remove all unnecessary function arguments

--- spin/ensemble.py
+++ spin/ensemble.py
@@ -8,16 +8,16 @@ class Ensemble(Operators):
     Sample system via MCMC with Gibbs Sampling
     """
 
-    def __init__(self, system, n_samples):
+    def __init__(self, system, n_samples=1):
 
-        self.configuration = self.sample(system.configuration,
-                system.geometry, system.energy, system.T,
-                n_samples)
+        self.__dict__.update(system.__dict__)
+        self.n_samples = n_samples
+        self.configuration = self.sample()
 
         # measure ensemble
         super(Ensemble, self).__init__()
     
-    def flip_spin(self, configuration, geometry):
+    def flip_spin(self):
 
         """
         Flip random spin on lattice
@@ -25,28 +25,28 @@ class Ensemble(Operators):
 
         # identify random index
         flip_indices = []
-        for dimension in geometry:
+        for dimension in self.geometry:
             if isinstance(dimension, int):
                 index = np.random.randint(dimension)
                 flip_indices.append(index)
         flip_indices = tuple(flip_indices)
 
-        configuration = copy.copy(configuration)
+        configuration = copy.copy(self.configuration)
         configuration[flip_indices] *= -1
         return configuration
     
-    def acceptance_criterion(self, energy_i, energy_f, T):
+    def acceptance_criterion(self, energy_f):
 
         """
         Gibbs acceptance
         """
 
-        energy_difference = energy_f - energy_i
-        gibbs_criterion = np.exp(-1. * energy_difference / T)
+        energy_difference = energy_f - self.energy
+        gibbs_criterion = np.exp(-1. * energy_difference / self.T)
         if (energy_difference < 0) or (np.random.rand() < gibbs_criterion):
             return True
     
-    def MC_step(self, configuration, geometry, energy, T):
+    def mc_step(self):
 
         """
         To take a step, flip a spin and check for acceptance
@@ -54,10 +54,10 @@ class Ensemble(Operators):
 
         while True:
             # flip spin
-            configuration_n = self.flip_spin(configuration, geometry)
-            energy_n = self.energy(configuration_n, J=-1.0)
+            configuration_n = self.flip_spin()
+            energy_n = self.measure_energy(configuration_n, J=-1.0)
             # accept according to acceptance criterion
-            if self.acceptance_criterion(energy, energy_n, T):
+            if self.acceptance_criterion(energy_n):
                 return configuration_n, energy_n
     
     def check_convergence(self, energies, threshold=.3):
@@ -67,7 +67,7 @@ class Ensemble(Operators):
         """
 
         ste = np.std(energies) / (len(energies)**.5)
-        if ste > threshold:
+        if ste < threshold:
             return True
     
     def check_autocorrelation(self, energies, threshold=.01):
@@ -84,8 +84,7 @@ class Ensemble(Operators):
             if ac < threshold:
                 return lag
     
-    def run_MCMC(self, configuration, geometry, energy, T, n_samples=1,
-            eq=False, min_steps=100):
+    def run_mcmc(self, eq=False, min_steps=100):
 
         """
         Generate samples
@@ -96,44 +95,32 @@ class Ensemble(Operators):
         configurations = []
         energies = []
         while True:
-            configuration, energy = self.MC_step(configuration, geometry,
-                    energy, T)
-            configurations.append(configuration)
-            energies.append(energy)
+            self.configuration, self.energy = self.mc_step()
+            configurations.append(self.configuration)
+            energies.append(self.energy)
             if len(energies) > min_steps:
                 if eq:
-                    if not self.check_convergence(energies):
-                        return configuration, energy
+                    if self.check_convergence(energies):
+                        break
                 else:
                     acl = self.check_autocorrelation(energies)
                     if acl != None:
                         id_configurations = configurations[::acl]
-                        if len(id_configurations) > n_samples:
-                            return id_configurations[:n_samples]
+                        if len(id_configurations) > self.n_samples:
+                            return id_configurations[:self.n_samples]
                 # if not converged/too correlated, run for 2x more steps
                 min_steps *= 2
 
-    def equilibrate(self, configuration, geometry, energy, T, n_samples):
-
-        """
-        Equilibrate (mix) the chain
-        """
-
-        return self.run_MCMC(configuration, geometry, energy, T, eq=True)
-
-    
-    def sample(self, configuration, geometry, energy, T, n_samples):
+    def sample(self):
 
         """
         Run MCMC scheme on initial configuration
         """
         
-        # equilibration
-        eq_configuration, eq_energy = self.equilibrate(configuration, geometry,
-                energy, T, n_samples)
+        # equilibrate (mix) the chain
+        self.run_mcmc(eq=True)
     
         # production, get at least n_samples samples
-        configurations = self.run_MCMC(eq_configuration, geometry, eq_energy,  
-                T, n_samples=n_samples)
+        configurations = self.run_mcmc()
     
         return np.array(configurations)
