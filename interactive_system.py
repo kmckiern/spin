e996936c37ca5044661f2a9deb261d26165fb1a5
kmckiern <kmckiern@stanford.edu>
2017-09-16 17:28:48 -0700

    updated and simpler interactive example

--- interactive_system.py
+++ interactive_system.py
@@ -20,31 +20,24 @@ else:
     x.generate_system(geometry=geo, T=temp)
     x.random_configuration()
 
-# measure observables of created system
-x.measure_system()
-
 # generate ensemble from system
 x.generate_ensemble(n_samples=5)
 
 # view ensemble configurations as np array
-ec = x._ensemble._configurations
-
-# measure observables on ensemble
-x.measure_ensemble()
+ec = x.ensemble.configuration
 
 # print descriptions of state and ensemble
-print('~ system description ~')
 x.describe_system()
 print ('\n')
-print('~ ensemble description ~')
 x.describe_ensemble()
+print ('\n')
 
 # create hopfield network
 x.generate_hopfield()
 
 # print description of network
-print ('~ network description ~')
 x.describe_network()
 
 # start interactive session to work with system/ensemble
 IPython.embed()
+
