afc5b89ae71a42e6472841904a677ef39b71c4c2
kmckiern <kmckiern@stanford.edu>
2017-10-03 15:36:34 -0700

    optimize rbm hyperparams, remove hopfield

--- spin/network.py
+++ spin/network.py
@@ -4,27 +4,6 @@ from sklearn.neural_network import BernoulliRBM
 from .operators import Operators
 
 
-def conditional_prob(a, b, epsilon):
-
-    """ P(a_i|b) """
-
-    return np.dot(a, b) + epsilon
-
-def sigmoid(x):
-
-    """ Common activation function, strangely not in numpy afaik """
-
-    return 1 / (1 + np.exp(-x))
-
-def binary_sig_prob(probs):
-
-    """ Apply activation to probabilities, and threshold """
-
-    activated = np.round(sigmoid(probs))
-    activated[activated == 0] = -1
-    return activated
-
-
 class Network(Operators):
 
     """ Base class for all network models """
@@ -42,43 +21,6 @@ class Network(Operators):
         self.train_data, self.test_data = train_test_split(self.data,
                 train_size=self.split_ratio)
 
-class Hopfield(Network):
-
-    """
-    Hopfield network model
-    doi:10.1073/pnas.79.8.2554 
-    http://web.cs.ucla.edu/~rosen/161/notes/hopfield.html
-
-    Parameters
-    ----------
-    data : binary arrays of spin configurations
-    split_ratio : test/train ratio
-    """
-
-    def __init__(self, data, split_ratio=.8):
-
-        super(Hopfield, self).__init__(data, split_ratio)
-        self.weights = np.zeros((self.n_neurons, self.n_neurons))
-
-        self.train()
-        self.test_errors = self.test()
-
-    def train(self):
-
-        """ Train weights according to generalized Hebbian rule """
-
-        for sample in self.train_data:
-            self.weights += np.outer(sample, sample)
-        # normalize, and zero diagonal
-        self.weights /= len(self.train_data)
-        np.fill_diagonal(self.weights, 0)
-
-    def test(self):
-
-        """ Use each sample to synchronously test the weight matrix """
-        
-        recall = np.sign(np.dot(self.test_data, self.weights))
-        return np.sum(recall != self.test_data, axis=1)
 
 class RestrictedBoltzmann(Network):
 
@@ -87,23 +29,50 @@ class RestrictedBoltzmann(Network):
     min(KL(P_h||P_v))
     """
 
-    def __init__(self, data, split_ratio=.8, learning_rate=[0.1, 0.01, .001],
-	n_hidden=None, n_iter=None):
+    def __init__(self, data, split_ratio=.8, 
+            batch_size = None,
+            learning_rate = [0.1, 0.01, .001],
+            n_iter = [100, 1000, 10000]):
+
+        self.rbm = None
 
         super(RestrictedBoltzmann, self).__init__(data, split_ratio)
 
-        self.learning_rate = learning_rate
-        if n_hidden is None:
-            n_hidden = int(self.n_neurons * .5)
-        self.n_hidden = n_hidden
-        self.n_iter = n_iter 
+        self.n_hidden = int(self.n_neurons * .5)
+        if batch_size is None:
+            batch_size = [2**i for i in range(2, int(self.n_hidden**.5)+1)]
 
-        self.rbm = self.build()
+        self.hypers = {'batch_size': batch_size,
+                  'learning_rate': learning_rate,
+                  'n_iter': n_iter}
 
-    def build(self, error_threshold=1.1):
+        self.build(optimize_h=True)
+
+    def optimize_hyperparams(self):
+
+        """ Optimize hyperparams, score using pseudo-likelihood """
+
+        import itertools
+
+        hyper_ps = sorted(self.hypers)
+        combs = list(itertools.product(*(self.hypers[name] for name in hyper_ps)))
+        scores = {}
+        for cndx, c in enumerate(combs):
+            sub_dict = dict(zip(hyper_ps, c))
+            rbm = BernoulliRBM(n_components=self.n_hidden, **sub_dict)
+            rbm.fit(self.train_data)
+            score = np.sum(rbm.score_samples(self.test_data))
+            scores[score] = sub_dict
+        best_score = max(scores.keys())
+        self.hypers = scores[best_score]
+
+    def build(self, optimize_h=False):
     
         """ Train weights via contrastive divergence """
 
-        rbm = BernoulliRBM(n_components=self.n_hidden, verbose=True)
+        if optimize_h:
+            self.optimize_hyperparams()
+        rbm = BernoulliRBM(n_components=self.n_hidden, verbose=True,
+                **self.hypers)
         rbm.fit(self.train_data)
-        return rbm
+        self.rbm = rbm
