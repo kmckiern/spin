440100af3fb55e06b04e96c52a21231271d5c389
kmckiern <kmckiern@stanford.edu>
2018-07-24 09:01:42 -0700

    model io functions

--- spin/model.py
+++ spin/model.py
@@ -1,15 +1,20 @@
+import os
+import pickle
+
 import numpy as np
+
 from spin.ensemble import run_mcmc
 
 
 class Model:
     """ Create, equilibrate, measure, and build network of model """
 
-    def __init__(self, J=1, T=1, geometry=(1,)):
+    def __init__(self, J=1, T=1, geometry=(1,), configuration=None, save_path='.'):
         self.J = J
         self.T = T
         self.geometry = geometry
-        self.configuration = None
+        self.configuration = configuration
+        self.save_path = save_path
 
     def random_configuration(self):
         """ Distribute particles according to random configuration """
@@ -30,3 +35,17 @@ class Model:
             self.configuration = run_mcmc(self.J, self.T, self.configuration)
         self.ensemble, self.energies = run_mcmc(self.J, self.T, self.configuration, ensemble_size)
 
+    def save_model(self, name='model.pkl'):
+        file_out = os.path.join(self.save_path, name)
+        if os.path.exists(file_out):
+            raise ValueError('model with this name already exists')
+        with open(file_out, 'wb') as f:
+            pickle.dump(self, f)
+
+    def load_model(self, name='model.pkl'):
+        if not os.path.exists(name):
+            raise ValueError('model does not exists')
+        with open(name, 'rb') as f:
+            obj = pickle.load(f)
+        for key in obj.__dict__:
+            setattr(self, key, obj.__dict__[key])
